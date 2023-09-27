45569a3043a332e231b4c1e0b7099f6308964769
vincentattafynn <vincentattafynn@gmail.com>
2023-09-27 18:02:33 +0000

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,11 @@
 const val ssnitPercentage: Double = 0.055
+val chargeableIncome = arrayOf(420.00, 110.00, 130.00, 3000.00, 16395.00, 29963.00, 50000.00)
+val rate = arrayOf(0.00, 0.05, 0.10, 0.175, 0.25, 0.30, 0.35)
+val tax_pay = arrayOf(0.00, 5.50, 13.00, 525.00, 4098.75, 8988.90, 17500.00)
+val cummIncome = arrayOf(412.00, 512.00, 642.00, 3642.00, 20037.00, 50000.00)
+val cummTax = arrayOf(0.00, 5.50, 18.50, 543.80, 4642.25, 13631.15)
+
+
 fun SSNIT_func(gross_income: Double): Double {
     //function to calculate amount paid to SSNIT
     val ssnitReturn = gross_income * ssnitPercentage
@@ -9,15 +16,45 @@ fun calculateTaxableIncome(ssnitReturn: Double, grossIncome: Double): Double {
     return grossIncome - ssnitReturn
 }
 
+fun getIncomeRange(grossIncome: Double, chargeableIncome: Array<Double>): Int {
+    var smallest_max: Double? = null
+    for (income in chargeableIncome) {
+        if (grossIncome > income) {
+            smallest_max = income
+        }
+    }
+    return chargeableIncome.indexOf(smallest_max)
+}
+
+fun calcTax(taxableIncome: Double, arrayIndex: Int, cummTax: Array<Double>, rate: Array<Double>): Double {
+    val remInc = taxableIncome - cummIncome[arrayIndex]
+    val temp: Double = (remInc * rate[arrayIndex + 1])
+    val finalTax = temp + cummTax[arrayIndex]
+    return finalTax
+}
+
+fun calcIncome(grossIncome: Double, amtToSsnit: Double, finalTax: Double): Double {
+    val temp = (grossIncome - amtToSsnit)
+    val finalIncome = temp - finalTax
+    return finalIncome
+}
+
 fun main() {
     println("Enter your Gross Income: ")
     val grossIncome: String? = readLine()
     val newGrossIncome = grossIncome?.toDouble()
     if (newGrossIncome != null) {
+
         val amtToSsnit = SSNIT_func(newGrossIncome)
-        val taxableIncome = calculateTaxableIncome(amtToSsnit,newGrossIncome)
-        println("$taxableIncome")
+        val taxableIncome = calculateTaxableIncome(amtToSsnit, newGrossIncome)
+        val incomeIndex: Int = getIncomeRange(taxableIncome, chargeableIncome)
+        val finalTax = calcTax(taxableIncome, incomeIndex, cummTax, rate)
+        val finalIncome = calcIncome(newGrossIncome, amtToSsnit, finalTax)
+
+
+        println("Your finalIncome is: ${finalIncome}")
         println("You'll pay $amtToSsnit to SSNIT")
+        println("And you'll pay ${finalTax} in tax")
     }
 }
 
