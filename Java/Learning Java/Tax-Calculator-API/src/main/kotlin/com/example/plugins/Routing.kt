541b0666c70758a0b18626623a961bef6d15fd7c
vincentattafynn <vincentattafynn@gmail.com>
2023-09-28 12:45:58 +0000

    first

---
+++ Java/Learning Java/Tax-Calculator-API/src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,16 @@
+package com.example.plugins
+
+import com.example.routes.customerRouting
+import com.example.routes.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        customerRouting()
+        listOrdersRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
+    }
+}
